d7b277f2558a6fc9361ecd5e34af1cf981f91ab0
kdahun <101082485+kdahun@users.noreply.github.com>
2024-03-19 12:53:21 +0900

    Add files via upload

---
+++ #7 DOCUMENTGPT/#7.0 Introduction/Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.title("Hello world")
+
+st.subheader("Welcome to Streamlit!")
+
+st.markdown("""
+### I love it!
+""")
---
+++ #7 DOCUMENTGPT/#7.0 Introduction/Home7.1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+
+st.write("hello")
+
+st.write({"x":1})
+
+st.write(PromptTemplate)
